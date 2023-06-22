11c7d9aa9bdd86063eb3f35b1087989de008dbdf
Devinson Barrios <devinson.barrios@gmail.com>
2023-06-22 17:03:23 -0500

    se crea un array con los mapas y se limpian y se obtiene los emojis que identifica cada letra en maps.js

--- game.js
+++ game.js
@@ -32,8 +32,15 @@ function startGame() {
     game.font= elementsZise + 'px Verdana';
     game.textAlign= 'end';
 
-    for (let i = 1; i <= 10 ; i++) {
-        game.fillText(emojis['X'], elementsZise , elementsZise * i);
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+    console.log({map,mapRows,mapRowCols});
+
+    for (let row = 1; row <= 10 ; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsZise * col , elementsZise * row);
+        } 
     }
 
     // window.innerHeight
