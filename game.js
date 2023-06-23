1f77020cb5d40ad5ca73b4e313e1268a32471ce7
Devinson Barrios <devinson.barrios@gmail.com>
2023-06-23 14:18:17 -0500

    se crean validaciones para que el jugador no salga de los bordes del mapa

--- game.js
+++ game.js
@@ -5,11 +5,17 @@ const btnLeft = document.querySelector('#left');
 const btnRight = document.querySelector('#right');
 const btnDown = document.querySelector('#down');
 
+let canvasSize;
+let elementsSize;
+
+const playerPosition ={
+    x: undefined,
+    y: undefined,
+}
+let playerMove = undefined;
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
-
-let canvasSize;
-let elementsZise;
 
 function setCanvasSize() {
     
@@ -22,7 +28,7 @@ function setCanvasSize() {
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
     
-    elementsZise = canvasSize / 10; 
+    elementsSize = canvasSize / 10; 
     
     startGame();
     
@@ -31,9 +37,9 @@ function setCanvasSize() {
 
 function startGame() {
 
-    console.log({canvasSize,elementsZise});
+    console.log({canvasSize,elementsSize});
 
-    game.font= elementsZise + 'px Verdana';
+    game.font= elementsSize + 'px Verdana';
     game.textAlign= 'end';
 
     const map = maps[0];
@@ -41,18 +47,29 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map,mapRows,mapRowCols});
 
+    // game.clearRect(0,0,canvasSize, canvasSize);
+    game.clearRect(0,0,canvasSize, canvasSize);
+
     mapRowCols.forEach((row, rowI) => { 
         row.forEach((col, colI)=>{
             const emoji = emojis[col];
-            const postX = elementsZise * (colI + 1);
-            const postY = elementsZise * (rowI + 1);
+            const postX = elementsSize * (colI + 1);
+            const postY = elementsSize * (rowI + 1);
+
+            if (col == 'O'){
+                if (!playerPosition.x && !playerPosition.y) { 
+                    playerPosition.x = postX; 
+                    playerPosition.y = postY;
+                    console.log({playerPosition});
+                }
+            }
             game.fillText(emoji, postX, postY);
         });
     });
 
     // for (let row = 1; row <= 10 ; row++) {
     //     for (let col = 1; col <= 10; col++) {
-    //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsZise * col , elementsZise * row);
+    //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col , elementsSize * row);
     //     } 
     // }
 
@@ -65,10 +82,15 @@ function startGame() {
     // game.fillStyle='purple';
     // game.textAlign ='left';
     // game.fillText('Platzi',5,25);
+
+    movePlayer();
+}
+
+function movePlayer() {
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
 window.addEventListener('keydown', moveByKeys);
-
 btnUp.addEventListener('click', moveUp);
 btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
@@ -84,16 +106,44 @@ function moveByKeys(event) {
 
 function moveUp() {
     console.log('mequiero mover hacia arriba');
+
+    if ((playerPosition.y - elementsSize) < (elementsSize -1)) {
+        console.log('out');
+    }else{
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
 }
 
 function moveLeft() {
     console.log('mequiero mover hacia izquierda');
+
+    if ((playerPosition.x - elementsSize) < (elementsSize)) {
+        console.log('out');
+    }else{
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
 }
 
 function moveRight() {
     console.log('mequiero mover hacia derecha');
+
+    if ((playerPosition.x + elementsSize) > (canvasSize + 1)) {
+        console.log('out');
+    }else{
+        playerPosition.x += elementsSize;
+        startGame();
+    }
 }
 
 function moveDown() {
     console.log('mequiero mover hacia abajo');
+
+    if ((playerPosition.y + elementsSize) > (canvasSize)) {
+        console.log('out');
+    }else{
+        playerPosition.y += elementsSize;
+        startGame();
+    }
 }
